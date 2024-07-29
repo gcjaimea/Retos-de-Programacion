ea3de4dd8858b033e4ec61e8f1cf80276914d093
gcjaime <gcjaime@gmail.com>
2024-07-29 10:10:16 -0500

    update

---
+++ Prueba0/reto0.py
@@ -0,0 +1,20 @@
+
+# Comentario de una sola línea
+
+# URL de python
+
+#otra forma de comentario en varias lineas
+'''
+Esta es la dirección de :
+
+https://www.python.org/
+
+'''
+# Variables
+x = "jaime"
+y =3
+n = [1,2,3,4,5]
+p = True
+
+# Mostrar Hola mundo
+print("Hola Python!!!")
--- Prueba2/reto2.py
+++ Prueba2/reto2.py
@@ -1,138 +1,107 @@
-# Operadores aritmeticos
-a = 10
-b= 3
+'''
+Crea ejemplos de funciones básicas que representen las diferentes
+*   posibilidades del lenguaje:
+*   Sin parámetros ni retorno, con uno o varios parámetros, con retorno...
+'''
 
-c = a + b    # operador de suma
-d = a - b    #operador de resta
-e = a * b    #operador de multilicacion
-f = a / b    # operador de division
-g = a // b   #division entera, muestra la parate entera solamente, descarta el residuo
-h = a & b    #division residuo, muestra solo el residuo
-i = a ** b   # operador portencia
+# Funcion sin parametros ni retornos
+def saludar():
+    print("Hola")
 
-#Operadores de asignacion
+saludar()
 
-j = a       # el valor de a se asigna a 'j'
-print (j)
 
-b += a      # el balor de b se le suma el valor de a
-print(b)
+#Funcion con parametros:
+def saludar_nombre(nombre):
+    print("Hola ", nombre)
 
-c -= b      #el valor de c se le resta el valor de b
-print(c)
+saludar_nombre("Jaime")
 
-e *= b      # el valor de e se le multiplica por el valor de b
-print(e)
+#Funcion con varios parametros y retorno
+def saludar_completo(nombre, apellido): 
+    return f"Hola {nombre} {apellido}"
 
-f /= b      # el valor de f se le divide por el valor de b
-print(f)
+saludo =saludar_completo("Jaime" , "Gonzalez")
+print(saludo)
 
-g %= b      # el valor de g se le  divide tipo residuo por el valor de b
-print(g)
 
-h **= b     # el valor de h se le eleva por el valor de b
-print(h)
+def sumar(n1,n2):
+    return n1+n2
 
-i //= b     # el valor de i se le divide tipo entero por el valor de b
-print(i)
-
-# Operadores de comparacion
-
-a == b      # se uasa para determinar la igualdad
-a != b      # variables diferentes
-a < b       # variable menor que
-a > b       # variable mayor que
-a >= b      # variable mayor igual que
-a <= b      # variable menor igual  que
+suma = sumar(1,2)
+print(suma)
 
 
 
-# Condicionales
-if a == b:
-    print('son iguales')
+def operaciones_matematicas(t, n1, n2):
+    if t.lower() == "s":
+        def sumar(n1, n2):
+            return n1 + n2
+        return sumar(n1, n2)
+    elif t.lower() == "r":
+        def restar(n1, n2):
+            return n1 - n2
+        return restar(n1, n2)
+    elif t.lower() == "m":
+        def multiplicar(n1, n2):
+            return n1 * n2
+        return multiplicar(n1, n2)
+    elif t.lower() == "d":
+        def dividir(n1, n2):
+            return n1 / n2
+        return dividir(n1, n2)
+    else:
+        return None
 
-else:
-    print('No son iguales')
+operacion = operaciones_matematicas("M", 2, 3)
 
-if a != b:
-    print("son diferentes")
-else:
-    print('son iguales')
-
-if a < b:
-    print('a es menor que b')
-else:
-    print('a es mayor que b')
-if a > b:
-    print('a es mayor que b')
-else:
-    print('a es menor que b')
-if a >= b:
-    print('a es mayor o igual  que b')
-else:
-    print('a es menor o igual que b')
-
-
-t =0
-while t < 20:
-    print(t)
-    t+=1
+print(operacion)
 
 
 
+#Ejemplo de variable local y Variable Global
 
-#Operadores Logicos
+variable_global = 5
 
-d = a and b     # operador and
-print(d)
+def variable_local():
+    variable_local1 = variable_global + 1
+    print(variable_local1)
+c = variable_local()
 
-e = a or b      # operador or
+#nuevo_numero = variable_local + 3 Da error...no reconoce el valor de la variable local
+print(variable_global)
 
-#a not b     # operador no
-
-a = True
-b = False
-c = True
-
-d = a and b    
-print(d)
-
-e = a or b     
-print(e)
-
-f = not a
-print(f)
+#print(nuevo_numero)   # No es posible accesar a una variable local!!
 
 
-# Iterativas
-nume = 0
-for i in range(5):
-    nume += i
-    print(nume)
 
-num = []
-for k in range(10):
-    num.append(k)
-print (num)
+# Dificultad Extra
+
+# Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
+
+cad1 = "Jaime"
+cad2 = "Carlos"
+            
+print("aqui!!!!")
+def retornar(cad1,cad2):
+    count = 0
+    for i in range (1,101):
+        if i % 3 ==0 and i %5 == 0:
+            print(cad1 + cad2)
+        elif i % 3 == 0:
+            print(cad1)
+        elif i % 5 == 0:
+            print(cad2)
+        else:
+            print(i)
+            count +=1
+    return count            
+        
+
+print(retornar(cad1,cad2))
 
 
-#Excepciones:
 
-try:
-    a =10/3
-except:
-    a > 1
-finally:
-    print("No se puede dividir, a es mayor de 1")
 
-numeros = list(range(10, 56))
-
-lista_numeros =[]
-
-for n in numeros:
-    if n % 2 == 0 and n !=16 and n % 3 != 0:
-        lista_numeros.append(n)
-         
-print(lista_numeros)
 
 
