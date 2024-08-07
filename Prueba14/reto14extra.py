471a07dbbae225987ec2c48bed97104b41850274
gcjaime <gcjaime@gmail.com>
2024-08-07 16:32:57 -0500

    update2

--- Prueba14/reto14extra.py
+++ Prueba14/reto14extra.py
@@ -14,15 +14,15 @@ fecha_cumpleaños_formateada8 = fecha_cumpleaños.strftime("%d/%m/%Y %I:%M:%S: %
 fecha_cumpleaños_formateada9 = fecha_cumpleaños.strftime("%d/%m/%Y %H:%M:%S")
 fecha_cumpleaños_formateada10 = fecha_cumpleaños.strftime("%d/%m/%Y %H:%M:%S.%f")
 
-print(f"Fecha Cumpleaños sin formatear: {fecha_cumpleaños}")
-print(f"Fecha Cumpleaños formateada1 : {fecha_cumpleaños_formateada1}")
-print(f"Fecha Cumpleaños formateada2 : {fecha_cumpleaños_formateada2}")
-print(f"Fecha Cumpleaños formateada3 : {fecha_cumpleaños_formateada3}")
-print(f"Fecha Cumpleaños formateada4 : {fecha_cumpleaños_formateada4}")
-print(f"Fecha Cumpleaños formateada5 : {fecha_cumpleaños_formateada5}")
-print(f"Fecha Cumpleaños formateada6 : {fecha_cumpleaños_formateada6}")
-print(f"Fecha Cumpleaños formateada7 : {fecha_cumpleaños_formateada7}")
-print(f"Fecha Cumpleaños formateada8 : {fecha_cumpleaños_formateada8}")
-print(f"Fecha Cumpleaños formateada9 : {fecha_cumpleaños_formateada9}")
-print(f"Fecha Cumpleaños formateada10 : {fecha_cumpleaños_formateada10}")
+print(f"Fecha Cumpleaños sin formatear: {fecha_cumpleaños}") # 1964-09-09 18:30:00
+print(f"Fecha Cumpleaños formateada1 : {fecha_cumpleaños_formateada1}") # 09-09-1964 18:30:00
+print(f"Fecha Cumpleaños formateada2 : {fecha_cumpleaños_formateada2}") # 09-09-1964 06:30:00: PM
+print(f"Fecha Cumpleaños formateada3 : {fecha_cumpleaños_formateada3}") # Wednesday-09-Sep-1964 18:30:00
+print(f"Fecha Cumpleaños formateada4 : {fecha_cumpleaños_formateada4}") # 1964-09-Wednesday-09 18:30:00
+print(f"Fecha Cumpleaños formateada5 : {fecha_cumpleaños_formateada5}") # 1964-09-Wednesday-09 06:30:00: PM
+print(f"Fecha Cumpleaños formateada6 : {fecha_cumpleaños_formateada6}") # 09-Sep-1964 06:30:00: PM
+print(f"Fecha Cumpleaños formateada7 : {fecha_cumpleaños_formateada7}") # 09-Sep-1964 18:30:00
+print(f"Fecha Cumpleaños formateada8 : {fecha_cumpleaños_formateada8}") # 09/09/1964 06:30:00: PM
+print(f"Fecha Cumpleaños formateada9 : {fecha_cumpleaños_formateada9}") # 09/09/1964 18:30:00
+print(f"Fecha Cumpleaños formateada10 : {fecha_cumpleaños_formateada10}") # 09/09/1964 18:30:00.000000
 
